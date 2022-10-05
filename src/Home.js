9950e16cac88fb2cdec085213ab1ccd016e0a3eb
Ambre G <guichard.ambr@gmail.com>
2022-10-05 22:36:09 +0200

    first commit test

---
+++ src/Home.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Coucou <code>ma bébé chat</code> de l'amour.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
